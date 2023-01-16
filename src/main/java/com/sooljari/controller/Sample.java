62a8092fc90bcbed0ce9fd369c14f5b72e16835d
hyexrin <ohr1003@gmail.com>
2023-01-16 14:54:51 +0900

    Test sample code

---
+++ src/main/java/com/sooljari/controller/Sample.java
@@ -0,0 +1,14 @@
+package com.sooljari.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Sample {
+
+    @RequestMapping("/sample")
+    public String greeting(){
+        return "sample!!";
+    }
+
+}
